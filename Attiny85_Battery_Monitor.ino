929894e2f307a1be4167fcdbf08d94c6217cdf54
flyonly <fourshorts@gmail.com>
2020-09-06 12:04:39 +1000

    Update Attiny85_Battery_Monitor.ino

--- Attiny85_Battery_Monitor.ino
+++ Attiny85_Battery_Monitor.ino
@@ -18,6 +18,31 @@ const int greenPin = PB0;
 const int bluePin = PB1;
 int del = 200; // delay
 
+// Example of sleeping and saving power
+//
+// Author: Nick Gammon
+// Date:   25 May 2011
+
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+
+// watchdog interrupt
+ISR(WDT_vect)
+  {
+  wdt_disable();  // disable watchdog
+  }
+
+void myWatchdogEnable(const byte interval)
+  { 
+  MCUSR = 0;                          // reset various flags
+  WDTCR |= 0b00011000;               // see docs, set WDCE, WDE
+  WDTCR =  0b01000000 | interval;    // set WDIE, and appropriate delay
+ 
+  wdt_reset();
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+  sleep_mode();            // now goes to Sleep and waits for the interrupt
+  }
+
 // Setup for outputs
 void setup()
 {
@@ -32,14 +57,15 @@ digitalWrite(greenPin, HIGH);
 delay(200);
 }
 
-void loop() {
+void loop(){
+ for (int x = 0; x < 5; x++){ // run for x seconds then sleep
  int sensorValue = analogRead(PB3);
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 4.9V):
   float voltage = sensorValue * (4.9 / 1023.0);
 delay(100);
  int value=sensorValue; 
 
-if (value<=550) {                                                                                                                                                                                                                                                                      
+if (value<=550)  {                                                                                                                                                                                                                                                                        
       digitalWrite(redPin, LOW);
       delay(del);
       digitalWrite(redPin, HIGH);
@@ -48,21 +74,21 @@ if (value<=550) {
       digitalWrite(greenPin, HIGH);
 }
 else 
-if (value>550 && value<=575) {   
-      digitalWrite(redPin,LOW);                                                     
+if (value>550 && value<=575) {
+    digitalWrite(redPin, LOW                                                                                                 );
       delay(del);
       digitalWrite(bluePin,HIGH);
       digitalWrite(greenPin,HIGH);
 }
 else 
-if (value>575 && value<=640) {    
+if (value>575 && value<=640) {
     digitalWrite(greenPin, LOW);
       delay(del);
       digitalWrite(redPin, HIGH);
       digitalWrite(bluePin, HIGH);
 }
 else 
-if (value>640 && value<=690) {    
+if (value>640 && value<=690) {
     digitalWrite(bluePin, LOW);
       delay(del);
       digitalWrite(redPin, HIGH);
@@ -79,5 +105,19 @@ if (value>690) {
       digitalWrite(greenPin, HIGH);
 }
   delay(100);
-}
-          
+  }
+  digitalWrite(redPin, HIGH);
+  digitalWrite(bluePin, HIGH);
+  digitalWrite(greenPin, HIGH);
+  
+  for (int i = 0; i < 6; i++){  // sleep for i times
+  myWatchdogEnable (0b100001);  // 8 second
+  }
+
+}  // end ofloop
+
+// sleep bit patterns:
+//  1 second:  0b000110
+//  2 seconds: 0b000111
+//  4 seconds: 0b100000
+//  8 seconds: 0b100001
